0a24e59acf0304c4303473a0aa62e8c459f4241a
Abir Modak <abrmod74@gmail.com>
2024-09-27 03:36:52 +0000

    adjusted preprocessing + created model

--- lib/setup_coco_json.py
+++ lib/setup_coco_json.py
@@ -33,21 +33,21 @@ def setup_rgb(folder_path, coco_json_dir, per_train, per_val, per_test):
 def setup_grayscale(folder_path, coco_json_dir, per_train, per_val, per_test):
     train_images, train_masks, val_images, val_masks, test_images, test_masks = preprocess_grayscale(folder_path, per_train, per_val, per_test)
 
-    save_images(train_images, os.path.join(coco_json_dir, 'grayscale', 'train', 'images'), base_filename='train_image')
+    save_images(train_images, os.path.join(coco_json_dir, 'grayscale', 'train', 'images'), base_filename='train_mask')
     save_images(train_masks, os.path.join(coco_json_dir, 'grayscale', 'train', 'masks', 'Tumor'), base_filename='train_mask')
-    save_images(val_images, os.path.join(coco_json_dir, 'grayscale', 'val', 'images'), base_filename='val_image')
+    save_images(val_images, os.path.join(coco_json_dir, 'grayscale', 'val', 'images'), base_filename='val_mask')
     save_images(val_masks, os.path.join(coco_json_dir, 'grayscale', 'val', 'masks', 'Tumor'), base_filename='val_mask')
-    save_images(test_images, os.path.join(coco_json_dir, 'grayscale', 'test', 'images'), base_filename='test_image')
+    save_images(test_images, os.path.join(coco_json_dir, 'grayscale', 'test', 'images'), base_filename='test_mask')
     save_images(test_masks, os.path.join(coco_json_dir, 'grayscale', 'test', 'masks', 'Tumor'), base_filename='test_mask')
 
 def setup_rgbd(folder_path, coco_json_dir, per_train, per_val, per_test):
     train_images, train_masks, val_images, val_masks, test_images, test_masks = preprocess_rgbd(folder_path, per_train, per_val, per_test)
 
-    save_images(train_images, os.path.join(coco_json_dir, 'rgbd', 'train', 'images'), base_filename='train_image')
+    save_images(train_images, os.path.join(coco_json_dir, 'rgbd', 'train', 'images'), base_filename='train_mask')
     save_images(train_masks, os.path.join(coco_json_dir, 'rgbd', 'train', 'masks', 'Tumor'), base_filename='train_mask')
-    save_images(val_images, os.path.join(coco_json_dir, 'rgbd', 'val', 'images'), base_filename='val_image')
+    save_images(val_images, os.path.join(coco_json_dir, 'rgbd', 'val', 'images'), base_filename='val_mask')
     save_images(val_masks, os.path.join(coco_json_dir, 'rgbd', 'val', 'masks', 'Tumor'), base_filename='val_mask')
-    save_images(test_images, os.path.join(coco_json_dir, 'rgbd', 'test', 'images'), base_filename='test_image')
+    save_images(test_images, os.path.join(coco_json_dir, 'rgbd', 'test', 'images'), base_filename='test_mask')
     save_images(test_masks, os.path.join(coco_json_dir, 'rgbd', 'test', 'masks', 'Tumor'), base_filename='test_mask')
 
 
