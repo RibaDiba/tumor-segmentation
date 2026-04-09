1abd2f6e6b91084bfb4fe92e401303cda8fcb4bc
Abir Modak <abrmod74@gmail.com>
2026-04-08 21:43:47 -0400

    fixed augmentations

--- src/util/preprocessing/augmentations.py
+++ src/util/preprocessing/augmentations.py
@@ -3,83 +3,77 @@ creating custom augmentations
 """
 
 import os, cv2
-from typing import List, Tuple 
-import numpy as np 
+from itertools import product
+from typing import List, Tuple
+import numpy as np
+import albumentations as A
 
 class AugmentationClass: 
 
     def __init__(
             self, 
             tumor_dataset, 
-            flip_prob: float, 
-            rotate_prob: float,
             rotate_degrees: int, 
-            target_size: int,
-            test_only: bool = False # TODO: implement this feature  
-    ): 
+            test_only: bool = False # TODO: implement this feature
+    ):
         """
-        Stores a reference to the tumor dataset structure, accesses its masks and images 
-        directly in order to augment the images to add more to our data 
+        Stores a reference to the tumor dataset structure, accesses its masks and images
+        directly in order to augment the images to add more to our data
 
         Args:
             tumor_dataset: reference to the tumor_dataset class
-            target_size: this is the size that our data is selected to be 
-            test_only: TODO, but this is to indicate if only the testing dataset should 
-            be altered    
+            test_only: TODO, but this is to indicate if only the testing dataset should
+            be altered
         """
 
         self.dataset = tumor_dataset
         self.test_only = test_only
-        self.flip_prob = flip_prob
-        self.rotate_prob = rotate_prob
         self.rotate_degrees = rotate_degrees
-        self.target_size = target_size
         
         # some defined constants 
         self.dataset_size = len(self.dataset.images_rgb)
 
     def augment_images(self) -> None:
 
-        if (self.target_size <= self.dataset_size): 
-            print(f"Target size of {self.target_size} <= dataset size of {self.dataset_size}, therefore no augmentation needed")
-            return 
+        """
+        we apply the following transformations to the dataset. Note that each transformation 
+        is applied and all possible combinations are applied to produce the most amount of 
+        unqiue images 
 
-        new_rgb, new_depth, new_rgd = [], [], []
-        new_masks = []
-        new_filenames = []
+        Vertical Flips 
+        Horizontal Flips 
+        Randomized Rotation 
 
-        i = 0
-        while (len(new_rgb) + self.dataset_size) < self.target_size:
-            idx = i % self.dataset_size
-            image_rgb = self.dataset.images_rgb[idx].copy()
+        """
+
+        self.new_rgb, self.new_depth, self.new_rgd = [], [], []
+        self.new_masks = []
+        self.new_filenames = []
+
+        for idx in range(self.dataset_size):
+            image_rgb   = self.dataset.images_rgb[idx].copy()
             image_depth = self.dataset.images_depth_maps[idx].copy()
-            image_rgd = self.dataset.images_rgd[idx].copy()
-
-            mask = self.dataset.masks[idx].copy()
-
+            image_rgd   = self.dataset.images_rgd[idx].copy()
+            mask        = self.dataset.masks[idx].copy()
             original_name = self.dataset.filenames[idx]
-
-            # now apply the transformation
-            image_rgb, image_depth, image_rgd, mask = self._flip_horizontally(image_rgb, image_depth, image_rgd, mask)
-            image_rgb, image_depth, image_rgd, mask = self._flip_vertically(image_rgb, image_depth, image_rgd, mask)
-            image_rgb, image_depth, image_rgd, mask = self._rotate(image_rgb, image_depth, image_rgd, mask)
-
-            # now save them to the array 
-            new_rgb.append(image_rgb)
-            new_depth.append(image_depth)
-            new_rgd.append(image_rgd)
-            new_masks.append(mask)
-            # change the filename as well 
             base, ext = os.path.splitext(original_name)
-            new_filenames.append(f"{base}_aug_{i}{ext}")
 
-            i += 1
+            # populates the new arrays 
+            self._return_combinations(
+                image_rgb, 
+                image_depth, 
+                image_rgd, 
+                mask, 
+                base,
+                ext
+            )
 
-        self.combined_rgb       = self.dataset.images_rgb        + new_rgb
-        self.combined_depth     = self.dataset.images_depth_maps + new_depth
-        self.combined_rgd       = self.dataset.images_rgd        + new_rgd
-        self.combined_masks     = self.dataset.masks              + new_masks
-        self.combined_filenames = self.dataset.filenames          + new_filenames
+        # merges all datasets togther
+        self.combined_rgb       = self.dataset.images_rgb        + self.new_rgb
+        self.combined_depth     = self.dataset.images_depth_maps + self.new_depth
+        self.combined_rgd       = self.dataset.images_rgd        + self.new_rgd
+        self.combined_masks     = self.dataset.masks              + self.new_masks
+        self.combined_filenames = self.dataset.filenames          + self.new_filenames
 
         # shuffle all parallel arrays with a single permutation to guarantee sync
         perm = np.random.permutation(len(self.combined_rgb))
@@ -89,6 +83,47 @@ class AugmentationClass:
         self.combined_masks     = [self.combined_masks[p]     for p in perm]
         self.combined_filenames = [self.combined_filenames[p] for p in perm]
 
+    def _return_combinations(
+        self, 
+        image_rgb, 
+        image_depth, 
+        image_rgd, 
+        mask,
+        base,
+        ext
+    ): 
+        """
+        returns images/masks that have gone under all possible augmentations
+        """
+
+        transforms = [
+            (self._flip_horizontally, "h"),
+            (self._flip_vertically,   "v"),
+            (self._rotate,            "r"),
+        ]
+
+        """
+        essentially this piece of code applies every combination of transformation 
+        using the itertool. This ensures the maximum amount of images generated for 
+        the dataset 
+        """
+        for combo in product([False, True], repeat=len(transforms)):
+            if not any(combo):
+                continue  # skip identity (no transform applied)
+
+            r, d, g, m = image_rgb.copy(), image_depth.copy(), image_rgd.copy(), mask.copy()
+            suffix = ""
+
+            for apply, (fn, tag) in zip(combo, transforms):
+                if apply:
+                    r, d, g, m = fn(r, d, g, m)
+                    suffix += tag
+
+            self.new_rgb.append(r)
+            self.new_depth.append(d)
+            self.new_rgd.append(g)
+            self.new_masks.append(m)
+            self.new_filenames.append(f"{base}_aug_{suffix}{ext}")
 
     def _flip_vertically(
         self, 
@@ -96,13 +131,53 @@ class AugmentationClass:
         depth_image, 
         rgd_image, 
         mask
-        ):
-        if np.random.random() < self.flip_prob:
-            rgb_image = cv2.flip(rgb_image, 0)
-            depth_image = cv2.flip(depth_image, 0)
-            rgd_image = cv2.flip(rgd_image, 0)
-            mask = cv2.flip(mask, 0)
+    ):
+        """
+        flips the image veritcally, since we are doing offline augmentations 
+        there is no probability on which image gets to be flipped or not
+        """
+        rgb_image = cv2.flip(rgb_image, 0)
+        depth_image = cv2.flip(depth_image, 0)
+        rgd_image = cv2.flip(rgd_image, 0)
+        mask = cv2.flip(mask, 0)
+
         return rgb_image, depth_image, rgd_image, mask
+    
+    def _elastic_transform(
+        self, 
+        rgb_image, 
+        depth_image, 
+        rgd_image, 
+        mask
+    ):   
+        """
+        uses the albumentations package to perform an elastic transform on the 
+        tumors  
+
+        TODO: chnage these constant values to variables to be passed through the 
+        pipeline 
+
+        TODO: finish implmenting this function -> not worth the time right now,
+        plus it might add too much noise to the images anyway
+        """
+        raise NotImplementedError("_elastic_transform is not yet implemented")
+
+        transform = A.ElasticTransform(
+            alpha=100,
+            sigma=10,
+            p=1.0  # always apply since you're controlling this offline
+        )
+
+        rgb_result = transform(image=rgb_image)
+        depth_result = transform(image=depth_image)
+        rgd_result = transform(image=rgd_image)
+
+        rgb_image = rgb_result['image']
+        depth_image = depth_result['image']
+        rgd_image = rgd_result['image']
+        
+        return rgb_image, depth_image, rgd_image, mask
+
 
     def _flip_horizontally(
         self, 
@@ -110,12 +185,16 @@ class AugmentationClass:
         depth_image, 
         rgd_image, 
         mask
-        ): 
-        if np.random.random() < self.flip_prob:
-            rgb_image = cv2.flip(rgb_image, 1)
-            depth_image = cv2.flip(depth_image, 1)
-            rgd_image = cv2.flip(rgd_image, 1)
-            mask = cv2.flip(mask, 1)
+    ): 
+        """
+        flips hte image horizontally, since we are doing offline augmentations
+        there is no porbability on which an image gets flipped 
+        """
+        rgb_image = cv2.flip(rgb_image, 1)
+        depth_image = cv2.flip(depth_image, 1)
+        rgd_image = cv2.flip(rgd_image, 1)
+        mask = cv2.flip(mask, 1)
+
         return rgb_image, depth_image, rgd_image, mask
 
     def _rotate(
@@ -124,15 +203,16 @@ class AugmentationClass:
         depth_image, 
         rgd_image, 
         mask
-        ): 
-        if np.random.random() < self.rotate_prob:
-            h, w = rgb_image.shape[:2]
-            angle = np.random.uniform(-self.rotate_degrees, self.rotate_degrees)
-            M = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1.0)
-            rgb_image = cv2.warpAffine(rgb_image, M, (w, h), borderMode=cv2.BORDER_REFLECT)
-            depth_image = cv2.warpAffine(depth_image, M, (w, h), borderMode=cv2.BORDER_REFLECT)
-            rgd_image = cv2.warpAffine(rgd_image, M, (w, h), borderMode=cv2.BORDER_REFLECT)
-            mask = cv2.warpAffine(mask, M, (w, h), borderMode=cv2.BORDER_REFLECT)
+    ): 
+
+        h, w = rgb_image.shape[:2]
+        angle = np.random.uniform(-self.rotate_degrees, self.rotate_degrees)
+        M = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1.0)
+        rgb_image = cv2.warpAffine(rgb_image, M, (w, h), borderMode=cv2.BORDER_REFLECT)
+        depth_image = cv2.warpAffine(depth_image, M, (w, h), borderMode=cv2.BORDER_REFLECT)
+        rgd_image = cv2.warpAffine(rgd_image, M, (w, h), borderMode=cv2.BORDER_REFLECT)
+        mask = cv2.warpAffine(mask, M, (w, h), borderMode=cv2.BORDER_REFLECT)
+
         return rgb_image, depth_image, rgd_image, mask
 
     def return_augmentations(self):
